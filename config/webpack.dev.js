e6e4def0896c14b41cd3cb0fcfb5abf962bdc514
ZhangMin1998 <1464253423@qq.com>
2023-10-29 00:40:37 +0800

    处理css、less、s[ac]ss、stylus

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -28,6 +28,63 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            // 配合.browserslistrc来指定兼容程度
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "postcss-preset-env", // 能解决大多数样式兼容性问题
+                ],
+              },
+            }
+          },
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            // 配合.browserslistrc来指定兼容程度
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "postcss-preset-env", // 能解决大多数样式兼容性问题
+                ],
+              },
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            // 配合.browserslistrc来指定兼容程度
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "postcss-preset-env", // 能解决大多数样式兼容性问题
+                ],
+              },
+            }
+          },
+          'stylus-loader'
+        ]
       }
       // 处理图片
       // 处理js
