76c0c5c2be55bd54f1832d34a9bd57857077ff52
ZhangMin1998 <1464253423@qq.com>
2023-10-29 00:36:46 +0800

    first commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,37 @@
+
+
+module.exports = {
+  entry: '.src/main.js',
+  output: {
+    path: undefined,
+    filename: 'static/js/[name].js',
+    chunkFilename: "static/js/[name].chunk.js",
+    assetModuleFilename: "static/js/[hash:8][ext][query]"
+  },
+  module:{
+    rules: [
+      // 处理css
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            // 配合.browserslistrc来指定兼容程度
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "postcss-preset-env", // 能解决大多数样式兼容性问题
+                ],
+              },
+            }
+          }
+        ]
+      }
+      // 处理图片
+      // 处理js
+    ]
+  },
+  plugins: []
+}
